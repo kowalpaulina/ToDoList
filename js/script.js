46cf0c2cfa1ed0a157c33559d960e3348ef10f65
Paulina Kowal <kowalpaulina@gmail.com>
2016-11-30 20:38:37 +0100

    Delete items, new style

--- js/script.js
+++ js/script.js
@@ -5,26 +5,25 @@ var todolist = document.getElementById("todo_task");
 var donelist = document.getElementById("done_task");
 var task = document.getElementById('text');
 
-window.onload = localstorage_getValue();
-
-var eventHandler = "click" || "ev.keyCode == 13";
-
-button.addEventListener("click",function(){
+button.addEventListener("click",function(event){
     var task = document.getElementById('text');
         if(!(task.value=='')){
             addTask(task);   
-            
         }else{
             addValidComment();
         }
-    //event.preventDefault();
+    event.preventDefault();
 });
 
 function cretateLi(string){
         var li = document.createElement("li");
-        var p = document.createElement("p");
-        p.innerHTML = string;
-        li.appendChild(p);     
+        var label = document.createElement("label");
+        label.innerHTML = string;
+        li.appendChild(label); 
+        var div = document.createElement("div");
+        div.innerHTML = "\u00D7";
+        div.classList = "close";
+        li.appendChild(div);
         return li;
     }
 
@@ -32,31 +31,31 @@ function addTask(){
     var task = document.getElementById('text');
     var taskItem = cretateLi(task.value);
     var p = todolist.appendChild(taskItem);
-    changeStatus(taskItem,completed);
+    changeStatus(taskItem, eventHandler=completed);
     task.value = "";
-    localstorage_setValue(p);
-}
-
+    //localstorage_setValue(p);
+    
+    var close = document.getElementsByClassName("close");
+    var i;
+    for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var element = this.parentElement;
+    element.style.display = "none";
+  }
+}}
 var completed = function(){
     var taskItem = this;
     donelist.appendChild(taskItem);
-    changeStatus(taskItem, incompleted);
-    localstorage_setValue(taskItem);
+    changeStatus(taskItem, eventHandler=incompleted);    
 }
-
-var incompleted = function(taskItem){
+var incompleted = function(){
     var taskItem = this;
     todolist.append(taskItem);
-    changeStatus(taskItem, completed);
-    localstorage_setValue();
+    changeStatus(taskItem, eventHandler=completed);
 }
-
-
 function changeStatus(item, eventHandler){
     item.onclick = eventHandler;
 }
-
-
 function addValidComment(){
     var comment = document.createElement("p"); 
     if(!(validation_box.hasChildNodes("p"))){
@@ -67,9 +66,8 @@ function addValidComment(){
         setTimeout(function(){validation_box.removeChild(comment)},2500);
         $(comment).fadeToggle(2500);
     }}
-  
-  
- function localstorage_setValue(p){
+
+ /*function localstorage_setValue(p){
     var todotask = $(p).val();
     console.log(todotask);
     var donetask = $(task).val();
@@ -84,5 +82,5 @@ function localstorage_getValue(){
     $('#todo_task li p').val(new_todotask);
     $('#done_task li p').val(new_donetask);
 }
-
+*/
 
